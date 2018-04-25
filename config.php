2d1d5d8c28733200fdb7ba4200354df1175575f0
Daniel Oduonye <contactdanny19@gmail.com>
2018-04-25 06:58:05 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,17 +4,11 @@
  *
  * Changes
  * -------
- * 09-Nov-2007 : Version 1.0, contributed by Niyi Gbodimowo;
- * 28-Feb-2008 : Version 2.0, contributed by Niyi Gbodimowo
- * $ID: $
- *
- * Trafficspaces Ad Server
  *
  * Copyright (c) 2007 Trafficspaces Inc. All rights reserved.
  */
 date_default_timezone_set('UTC');
 
-//define('Temp_Dir', 'file://C:/tmp/log/pfabackoffice/media/');
 define('Temp_Dir', '/tmp');
 
 define('Content_Type_JAVASCRIPT',	'application/javascript');
@@ -22,19 +16,18 @@ define('Content_Type_JSON',	'application/json');
 define('Content_Type_PLAIN',	'text/plain');
 
 // Database-related configuration
-//define('Database_DSN', 'sqlsrv:server=192.168.1.52\SQLEXPRESS;Database=STANBICIBTC');
-define('Database_DSN', 'dblib:host=\SQLEXPRESS;dbname=STANBICIBTC');
+define('Database_DSN', '');
 define('Database_Username', 'sa');
 define('Database_Password', '');
 
-define('Employees_Table_Catalog', 'STANBICIBTC');
-define('Employees_Table_Schema', 'dbo');
-define('Employees_Table_Name', 'EMPLOYEES');
+define('Employees_Table_Catalog', '');
+define('Employees_Table_Schema', '');
+define('Employees_Table_Name', '');
 
-define('Verity_Image_Webroot', 'http://10.234.240.48/apps/');
-define('Verity_Documents_Webroot', 'http://localhost/uploads/documents/');
+define('Verity_Image_Webroot', '');
+define('Verity_Documents_Webroot', '');
 
-define('Employee_Media_Webroot', 'http://localhost/uploads/media/');
+define('Employee_Media_Webroot', '');
 
 define('Employee_Media_Passport_Width', 120);
 define('Employee_Media_Passport_Height', 140);
@@ -43,10 +36,9 @@ define('Employee_Media_Signature_Height', 120);
 define('Employee_Media_Biometrics_Width', 120);
 define('Employee_Media_Biometrics_Height', 140);
 
-define('API_Username', 'sipml');
-define('API_Password', 'abcdefghijklmnopqwsrtuvwzyz');
-define('API_Endpoint_Employees', 'http://sipml.backoffice.ng/api/contributor-registrations');
-//define('API_Endpoint_Employees', 'http://requestb.in/r677v6r6');
+define('API_Username', '');
+define('API_Password', '');
+define('API_Endpoint_Employees', '');
 define('API_PageSize', 25);
 define('Database_BatchSize', 100);
 
